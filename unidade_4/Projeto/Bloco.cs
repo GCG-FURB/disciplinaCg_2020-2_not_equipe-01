9cffc952febedf4a9ef460b8b0a7f02b0a2bb21e
Bruno Henrique de Borba <brunohenriqueborba@gmail.com>
2020-12-12 16:13:12 -0300

    Criada rotacao do objeto I

--- unidade_4/Projeto/Bloco.cs
+++ unidade_4/Projeto/Bloco.cs
@@ -7,28 +7,31 @@ namespace CG_N3
 {
     internal class Bloco : Retangulo
     {
-        public Bloco(char rotulo, Objeto paiRef, Ponto4D ptoInfEsq, Ponto4D ptoSupDir, BlocoType blocoType, string mode) : base(rotulo, paiRef, ptoInfEsq, ptoSupDir)
+        public Bloco(char rotulo, Objeto paiRef, Ponto4D ptoInfEsq, Ponto4D ptoSupDir, BlocoType blocoType) : base(rotulo, paiRef, ptoInfEsq, ptoSupDir)
         {
             base.PontosAdicionar(ptoInfEsq);
             base.PontosAdicionar(new Ponto4D(ptoSupDir.X, ptoInfEsq.Y));
             base.PontosAdicionar(ptoSupDir);
             base.PontosAdicionar(new Ponto4D(ptoInfEsq.X, ptoSupDir.Y));
             BlocoType = blocoType;
-            Mode = mode;
+            Mode = "Mode1";
 
             switch (BlocoType)
             {
                 case BlocoType.T:
                     this.GenerateT();
                     break;
+                case BlocoType.I:
+                    this.GenerateI();
+                    break;
                 default:
                     break;
             }
         }
 
-        public BlocoType BlocoType { get; set; }
+        public BlocoType BlocoType { get; }
 
-        public string Mode { get; set; }
+        internal string Mode { get; set; }
 
         public void GenerateT()
         {
@@ -38,6 +41,14 @@ namespace CG_N3
             this.FilhoAdicionar(new Retangulo('B', this, new Ponto4D(pto.X, pto.Y - 30), new Ponto4D(pto.X + 30, pto.Y)));
         }
 
+        public void GenerateI()
+        {
+            Ponto4D pto = pontosLista[0];
+            this.FilhoAdicionar(new Retangulo('A', this, new Ponto4D(pto.X, pto.Y + 30), new Ponto4D(pto.X + 30, pto.Y + 60)));
+            this.FilhoAdicionar(new Retangulo('B', this, new Ponto4D(pto.X, pto.Y - 30), new Ponto4D(pto.X + 30, pto.Y)));
+            this.FilhoAdicionar(new Retangulo('C', this, new Ponto4D(pto.X, pto.Y - 60), new Ponto4D(pto.X + 30, pto.Y - 30)));
+        }
+
         public void Rotate()
         {
             switch (BlocoType)
@@ -45,12 +56,15 @@ namespace CG_N3
                 case BlocoType.T:
                     this.RotateT();
                     break;
+                case BlocoType.I:
+                    this.RotateI();
+                    break;
                 default:
                     break;
             }
         }
 
-        public void RotateT()
+        private void RotateT()
         {
             foreach (var filho in this.GetFilhos())
             {
@@ -91,6 +105,79 @@ namespace CG_N3
                     default:
                         break;
                 }
+            }
+        }
+
+        private void RotateI()
+        {
+            switch (this.Mode)
+            {
+                case "Mode1":
+                    foreach (var filho in this.GetFilhos())
+                    {
+                        switch (filho.rotulo)
+                        {
+                            case 'A':
+                                foreach (var pto in filho.pontosLista)
+                                {
+                                    pto.X -= 30;
+                                    pto.Y -= 30;
+                                }
+                                break;
+                            case 'B':
+                                foreach (var pto in filho.pontosLista)
+                                {
+                                    pto.X += 30;
+                                    pto.Y += 30;
+                                }
+                                break;
+                            case 'C':
+                                foreach (var pto in filho.pontosLista)
+                                {
+                                    pto.X += 60;
+                                    pto.Y += 60;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    this.Mode = "Mode2";
+                    break;
+                case "Mode2":
+                    foreach (var filho in this.GetFilhos())
+                    {
+                        switch (filho.rotulo)
+                        {
+                            case 'A':
+                                foreach (var pto in filho.pontosLista)
+                                {
+                                    pto.X += 30;
+                                    pto.Y += 30;
+                                }
+                                break;
+                            case 'B':
+                                foreach (var pto in filho.pontosLista)
+                                {
+                                    pto.X -= 30;
+                                    pto.Y -= 30;
+                                }
+                                break;
+                            case 'C':
+                                foreach (var pto in filho.pontosLista)
+                                {
+                                    pto.X -= 60;
+                                    pto.Y -= 60;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    this.Mode = "Mode1";
+                    break;
+                default:
+                    break;
             }
         }
     }
